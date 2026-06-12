df6ff18d155a0713af029e5b809778942b592ad8
Jamtit <majauskas.titas@gmail.com>
2026-06-12 20:33:14 +0300

    feat: configure initial store & provide the store to the whole app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app/styles/global.scss";
 import App from "./app/App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
