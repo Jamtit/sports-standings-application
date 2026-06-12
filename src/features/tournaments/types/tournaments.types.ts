ff61dae0dfe59131abde5e55f99ddf1aed4e6778
Jamtit <majauskas.titas@gmail.com>
2026-06-12 19:36:46 +0300

    chore: add teams or player type named participant

--- src/features/tournaments/types/tournaments.types.ts
+++ src/features/tournaments/types/tournaments.types.ts
@@ -3,14 +3,30 @@ export type TournamentTypes = "premier-league" | "eurobasket" | "wimbledon";
 export type Tournament = {
   type: TournamentTypes;
   name: string;
-  participants: number;
+  participants: Participant[];
   matches: Match[];
 };
 
 export type Match = {
   id: string;
-  participantA: string;
-  participantB: string;
+  participantA: Participant["name"];
+  participantB: Participant["name"];
   scoreA: number;
   scoreB: number;
+};
+
+export type Participant = {
+  id: string;
+  name: string;
+  country?: string;
+};
+
+export type ParticipantStats = {
+  participantId: string;
+  name: string;
+  matchesPlayed: number;
+  wins: number;
+  losses: number;
+  draws: number;
+  points: number;
 };
