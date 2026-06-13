02a714b0c26c114e795b5f5b13e29bdbb9e660fb
Jamtit <majauskas.titas@gmail.com>
2026-06-13 16:21:44 +0300

    feat: create a container to hold tournament cards

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,12 @@
 import type { RootState, AppDispatch } from "./store";
 import { useDispatch, useSelector } from "react-redux";
+import type { TournamentTypes } from "../features/tournaments/types/tournaments.types";
 
-export const useAppDispatch = () => useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = () => useSelector.withTypes<RootState>();
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
+
+export const useTournamentTypes = () => {
+  return useAppSelector(
+    (state) => Object.keys(state.tournaments.tournaments) as TournamentTypes[],
+  );
+};
