4643c6583536f6c7521aeee1b2a01a483d3d6a4f
Jamtit <majauskas.titas@gmail.com>
2026-06-17 02:28:31 +0300

    chore: mock redux store & other apis

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,20 @@
 import "@testing-library/jest-dom/vitest";
+import { afterEach, beforeEach, vi } from "vitest";
+
+let uuidIndex = 0;
+
+beforeEach(() => {
+  uuidIndex = 0;
+
+  vi.spyOn(crypto, "randomUUID").mockImplementation(() => {
+    uuidIndex += 1;
+
+    return `00000000-0000-4000-8000-${uuidIndex
+      .toString()
+      .padStart(12, "0")}`;
+  });
+});
+
+afterEach(() => {
+  vi.restoreAllMocks();
+});
